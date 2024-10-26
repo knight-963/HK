72e37176bdf390880ec176ead1fed271f72265e0
Muhammad Amirxon <muhammadamirxon14@gmail.com>
2024-10-26 17:19:13 +0500

    salom

--- components/Comp.jsx
+++ components/Comp.jsx
@@ -20,16 +20,31 @@ export default function comp() {
         </div>
 
 <h1 className="text-5xl text-sky-500 pl-[110px] p-[20px] pt-[200px]">Actualni</h1>
-        <div className="flex justify-center gap-[50px] pt-[20px]">
+        <div className="flex justify-center gap-[50px] pt-[20px] bg-emerald-400 h-[550px]">
 
         <div class="card1">
           <img src="f1.jpg" className="im" alt="" />
-          <a href=""><h1 className="text-3xl text-sky-500 text-center p-[20px]">Suv ifloslanishi</h1></a>
+          <a href=""><h1 className="text-4xl text-sky-500 text-center p-[30px]">Suv ifloslanishi</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[10px] text-xl">Muammosi haqida</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[2px] text-xl">va</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[10px] text-xl">Yechilishi</h1></a>
         </div>
 
-        <div class="card1"></div>
+        <div class="card1">
+          <img src="f2.jpg" className="im" alt="" />
+          <a href=""><h1 className="text-4xl text-sky-500 text-center p-[30px]">Suv qimmatligi</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[10px] text-xl">Muammosi haqida</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[2px] text-xl">va</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[10px] text-xl">Yechilishi</h1></a>
+        </div>
 
-        <div class="card1"></div>
+        <div class="card1">
+          <img src="f3.jpg" className="im h-[230px] w-[100%]" alt="" />
+          <a href=""><h1 className="text-4xl text-sky-500 text-center p-[30px]">Suv kamayib borishi</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[10px] text-xl">Muammosi haqida</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[2px] text-xl">va</h1></a>
+          <a href=""><h1 className="text-center text-emerald-500 p-[10px] text-xl">Yechilishi</h1></a>
+        </div>
 
         </div>
 
